71b0dd2d2d1b84fa8c9d2cecdd811dc0b2aa5ba3
Alexey Ershov <alaershov@gmail.com>
2019-08-31 02:17:59 +0700

    Fix Retrofit interface to use form-url-encoded body and suspend function

--- app/src/main/java/com/mdgroup/beautyroom/data/api/BeautyRoomApiService.kt
+++ app/src/main/java/com/mdgroup/beautyroom/data/api/BeautyRoomApiService.kt
@@ -1,14 +1,16 @@
 package com.mdgroup.beautyroom.data.api
 
-import com.mdgroup.beautyroom.data.api.model.SignInApiModel
 import com.mdgroup.beautyroom.domain.model.ServerSignInResult
-import retrofit2.http.Headers
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface BeautyRoomApiService {
 
     @POST("authenticate")
-    @Headers("Content-Type: application/x-www-form-urlencoded")
-    fun signIn(mapToSignInApiModel: SignInApiModel): ServerSignInResult
-
+    @FormUrlEncoded
+    suspend fun signIn(
+        @Field("login") login: String,
+        @Field("pass") password: String
+    ): ServerSignInResult
 }
