c0284e10ab6f1915b143735a02943b781d5dec62
katkoff <katkov@improveitgroup.com>
2019-09-17 18:42:58 +0700

    Restyled master list item

--- app/src/main/java/com/mdgroup/beautyroom/ui/master/list/adapter/MasterListItemViewHolder.kt
+++ app/src/main/java/com/mdgroup/beautyroom/ui/master/list/adapter/MasterListItemViewHolder.kt
@@ -2,6 +2,7 @@ package com.mdgroup.beautyroom.ui.master.list.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mdgroup.beautyroom.R
 import com.mdgroup.beautyroom.domain.model.Master
 import kotlinx.android.extensions.LayoutContainer
@@ -11,19 +12,45 @@ class MasterListItemViewHolder(override val containerView: View) : RecyclerView.
     LayoutContainer {
 
     fun bind(master: Master, onItemClicked: (Int) -> Unit) {
-        textView_firstName.text = master.firstName.ifEmpty {
+        Glide.with(itemView)
+            .load(master.avatarUrl)
+            .placeholder(R.drawable.avatar_placeholder)
+            .into(circleImageView_avatar)
+
+        val fullName = "${master.firstName} ${master.lastName}"
+        textView_masterName.text = fullName.ifEmpty {
             itemView.resources.getString(R.string.unknown_placeholder)
         }
-        textView_lastName.text = master.lastName.ifEmpty {
-            itemView.resources.getString(R.string.unknown_placeholder)
-        }
+
         textView_phone.text = master.mobilePhone.ifEmpty {
             itemView.resources.getString(R.string.unknown_placeholder)
         }
+
         textView_info.text = master.information.ifEmpty {
             itemView.resources.getString(R.string.unknown_placeholder)
         }
 
+        val servicesString = getMasterServicesString(master)
+        textView_services.text = servicesString.ifEmpty {
+            itemView.resources.getString(R.string.unknown_placeholder)
+        }
+
         itemView.setOnClickListener { onItemClicked.invoke(master.id) }
+    }
+
+    private fun getMasterServicesString(master: Master): String {
+        var servicesString = ""
+        master.services.forEachIndexed { index, serviceItem ->
+            servicesString = when (index) {
+                0 -> if (master.services.size == 1) {
+                    serviceItem.name
+                } else {
+                    "${serviceItem.name}, "
+                }
+                master.services.lastIndex -> servicesString.plus(serviceItem.name)
+                else -> servicesString.plus("${serviceItem.name}, ")
+            }
+        }
+        return servicesString
     }
 }
